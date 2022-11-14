3d51140fc351a3dab9746bdeb89b7c3004a53ff9
Sachin Sourabh <Sachin.Sourabh@MSBC-MAINFRAME.LCL>
2022-11-14 13:43:52 +0530

    Updates

---
+++ precache-manifest.76108222ea5b7fbaf32a4fbfc1343006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "acdb9dba70c4ad292145",
+    "url": "/PuneMetroReact/static/css/main.ec462284.chunk.css"
+  },
+  {
+    "revision": "acdb9dba70c4ad292145",
+    "url": "/PuneMetroReact/static/js/main.acdb9dba.chunk.js"
+  },
+  {
+    "revision": "093ecf0bec58628b4322",
+    "url": "/PuneMetroReact/static/js/1.093ecf0b.chunk.js"
+  },
+  {
+    "revision": "fa71694a76fb9892aa30",
+    "url": "/PuneMetroReact/static/js/2.fa71694a.chunk.js"
+  },
+  {
+    "revision": "d1a8adcc080805ef7550",
+    "url": "/PuneMetroReact/static/js/runtime~main.d1a8adcc.js"
+  },
+  {
+    "revision": "394ff4c248630cf6c0dad9e811239d24",
+    "url": "/PuneMetroReact/index.html"
+  }
+];
